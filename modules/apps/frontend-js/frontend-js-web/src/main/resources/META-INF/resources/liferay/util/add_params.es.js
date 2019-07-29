6ffce3ececf7e53f997461e780b158d0b3935c4a
mateomustapic <mateo.mustapic@liferay.com>
2019-07-29 14:43:03 +0200

    LPS-95863 Move addParams utility to frontend-js-web

---
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/util/add_params.es.js
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+/**
+ * Appends given parameters to the given URL.
+ * @param {String | Object} params Parameters to be added on to the base url
+ * @param {String} url Base url
+ * @return {String} finalUrl URL constructed from base url and params
+ * @review
+ */
+export default function addParams(params, url) {
+	if (typeof params !== 'object' && typeof params !== 'string') {
+		throw new TypeError('Parameter params must be an object or string');
+	}
+
+	if (typeof url !== 'string') {
+		throw new TypeError('Parameter url must be a string');
+	}
+
+	if (typeof params === 'object') {
+		const paramKeys = Object.keys(params);
+
+		params = paramKeys
+			.map(function(key) {
+				return `${encodeURIComponent(
+					key
+				)}=${encodeURIComponent(params[key])}`;
+			})
+			.join('&');
+	} else {
+		params = params.trim();
+	}
+
+	let loc = url || location.href;
+
+	let finalUrl = loc;
+
+	if (params) {
+		let anchorHash;
+
+		if (loc.indexOf('#') > -1) {
+			const locationPieces = loc.split('#');
+
+			loc = locationPieces[0];
+			anchorHash = locationPieces[1];
+		}
+
+		if (loc.indexOf('?') === -1) {
+			params = `?${params}`;
+		} else {
+			params = `&${params}`;
+		}
+
+		if (loc.indexOf(params) === -1) {
+			finalUrl = loc + params;
+
+			if (anchorHash) {
+				finalUrl += `#${anchorHash}`;
+			}
+			if (!url) {
+				location.href = finalUrl;
+			}
+		}
+	}
+
+	return finalUrl;
+}
