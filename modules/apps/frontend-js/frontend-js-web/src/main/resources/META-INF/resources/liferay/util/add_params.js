b5b6cdeb004f87df6aebd1cb964767ae58730fc5
mateomustapic <mateo.mustapic@liferay.com>
2020-04-20 14:41:13 +0200

    LPS-101640 Vanilla-JS replacement for an old addParams API

---
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/util/add_params.js
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+/**
+ * Appends given parameters to the given URL.
+ * @param {String | Object} params Parameters to be added on to the base url
+ * @param {String} baseUrl Base url
+ * @return {String} url URL constructed from base url and params
+ * @review
+ */
+export default function addParams(params, baseUrl) {
+	if (typeof params !== 'object' && typeof params !== 'string') {
+		throw new TypeError('Parameter params must be an object or string');
+	}
+
+	if (typeof baseUrl !== 'string') {
+		throw new TypeError('Parameter baseUrl must be a string');
+	}
+
+	const url = new URL(baseUrl);
+
+	if (params && typeof params === 'object') {
+		Object.keys(params).map(key => {
+			url.searchParams.append(key, params[key]);
+		});
+	}
+	else {
+		params = params.trim();
+		url.searchParams.append(params, '');
+	}
+
+	return url.toString();
+}
